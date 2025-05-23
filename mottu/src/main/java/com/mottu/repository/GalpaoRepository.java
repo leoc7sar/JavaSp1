85b2df55a8a83978e10699a366a620748afd9b02
leoc7sar <161585547+leoc7sar@users.noreply.github.com>
2025-05-23 08:38:17 -0300

    Delete mottu directory

--- mottu/src/main/java/com/mottu/repository/GalpaoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mottu.repository;
-
-import com.mottu.model.Galpao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GalpaoRepository extends JpaRepository<Galpao, Long> {
-}
--- mottu/src/main/resources/Import.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO moto (id, modelo, placa, disponibilidade, gal) VALUES ('1', 'Honda', 'BGT7890', 'Disponivel');
-INSERT INTO moto (id, modelo, placa, disponibilidade) VALUES ('2', 'Suzuki', 'ZCX1234', 'Indisponivel');
-INSERT INTO moto (id, modelo, placa, disponibilidade) VALUES ('3', 'Kawasaki', 'MNO5678', 'Disponivel');
-INSERT INTO moto (id, modelo, placa, disponibilidade) VALUES ('4', 'Ducati', 'QWE4321', 'Indisponivel');
-INSERT INTO moto (id, modelo, placa, disponibilidade) VALUES ('5', 'BMW', 'TDH9876', 'Disponivel');
-
-SELECT * FROM motos;
